330d78077f28898cd34060812d855a7581086444
Mage-M <mmxxtt1234@126.com>
2015-10-11 16:09:55 +0800

    游戏基本完成

--- src/com/mage/game2048/MainActivity.java
+++ src/com/mage/game2048/MainActivity.java
@@ -3,16 +3,45 @@ package com.mage.game2048;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+
 import com.example.game2048.R;
 public class MainActivity extends Activity{
 
+	public MainActivity() {
+		mainActivity = this;
+	}
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+		tvScore = (TextView)findViewById(R.id.tvScore);
 	}
 
+	public void clearScore() {
+		score = 0;
+		showScore();
+	}
 	
+	public void showScore() {
+		tvScore.setText(score + "");
+	}
+	
+	public void addScore(int s) {
+		score += s;
+		showScore();
+	}
+	
+	private int score = 0;
+	
+	private TextView  tvScore;
+	
+	
+	private static MainActivity mainActivity = null;
+	
+	public static MainActivity getMainActivity() {
+		return mainActivity;
+	}
 }
