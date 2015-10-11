d34f68301b6d0af3b3531eaa5a93fb23f12575bd
Mage-M <mmxxtt1234@126.com>
2015-10-11 13:21:49 +0800

    first commit

---
+++ src/com/mage/game2048/MainActivity.java
@@ -0,0 +1,18 @@
+package com.mage.game2048;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.example.game2048.R;
+public class MainActivity extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+	}
+
+	
+}
